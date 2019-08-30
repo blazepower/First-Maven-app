c3bb5a86e86435410f8cb6870608e0d97dd40452
Rishik Hombal <hombalrishik@gmail.com>
2019-08-30 13:54:14 -0400

    Update to pom.xml

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -1,4 +1,18 @@
 package test.java.hello;
 
+import main.java.hello.Greeter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class GreeterTest {
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterHelloWorld(){
+        String s = "Hello World";
+
+        Assert.assertEquals(s, greeter.sayHello());
+    }
 }
